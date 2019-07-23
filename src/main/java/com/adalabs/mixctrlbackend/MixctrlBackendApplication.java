98b0bc1fc51a4aa113af2a0241e00dc0e7e7879f
Alex Guerra <alex@ada-labs.com>
2019-07-23 18:23:14 -0500

    Initial commit

---
+++ src/main/java/com/adalabs/mixctrlbackend/MixctrlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adalabs.mixctrlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixctrlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixctrlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adalabs/mixctrlbackend/MixctrlBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adalabs.mixctrlbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MixctrlBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
